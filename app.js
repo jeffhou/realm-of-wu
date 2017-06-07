d945490029e43e0d5aa8b7fa1a84a7666cadcb79
Jeff Hou <j.king.hou@gmail.com>
2017-06-07 15:35:09 -0700

    Added Pug, basic styling and templates

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+const pug = require('pug')
+const compiledTemplate = pug.compileFile('templates/index.pug');
+const compiledTemplate2 = pug.compileFile('templates/basic-template.pug');
+
 const express = require('express')
 const app = express()
 
+app.use('/css', express.static('templates/css'))
+
 app.get('/', function (request, response) {
-  response.send('Hello World')
+  response.send(compiledTemplate({name: 'Jeff'}))
 })
+
+app.get('/test', function (request, response) {
+  response.send(compiledTemplate2({name: 'Jeff'}))
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
